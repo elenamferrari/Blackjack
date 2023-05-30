d58b667e8508861717a106b916977c970fa896b3
len10101 <117754846+len10101@users.noreply.github.com>
2023-05-30 11:24:46 -0400

    Created Blackjack project.

---
+++ src/Card.java
@@ -0,0 +1,13 @@
+public class Card {
+    public String suit;
+    public int value;
+    public String name;
+    public boolean isDealt;
+
+
+    public Card (int pSuit, int pName){
+        if(pSuit==0) {
+            suit="Diamond";
+        }
+    }
+}
